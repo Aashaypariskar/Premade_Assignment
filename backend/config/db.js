22850260a1043cbe289b3b02bdf5c2afc1a5857d
Aashay Pariskar <aashaypariskar1605@gmail.com>
2026-02-20 15:37:11 +0530

    feat: add Sequelize database connection configuration for MySQL.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 // Replace with your real MySQL credentials
-const sequelize = new Sequelize('inspection_db', 'root', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize('inspection_db', 'railway_user', 'Railway@123', {
+    host: '3.110.16.111',
     dialect: 'mysql',
     logging: false,
     port: 3307
